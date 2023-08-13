c118cae58276948006c68531187589844584db66
Oluwapelumi Olayiwola <Olayiwolapelumi14@yahoo.com>
2023-08-13 13:39:12 -0700

    prints all single digit numbers of base 10 starting from 0

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * Main - Entry point
+ *
+ * Return: Always Success (0)
+ */
+
+int main(void)
+{
+	int i = 0;
+	for (i = 0; i < 10 ; i++)
+	{
+	printf(i);
+	}
+	return (0);
+}
